e4bce9bf2e8097c56a7c624ced9d3f4e7ab6352f
MohammadDallash <105324962+MohammadDallash@users.noreply.github.com>
2022-05-16 12:56:12 +0200

    kinda working

--- src/main/java/Dallash/main.java
+++ src/main/java/Dallash/main.java
@@ -9,52 +9,60 @@ import java.util.Stack;
 
 public class main {
 
+
+    static String Cureent_cell;
+    static String type;
+    static String last_value;
+    static int i = 0;
+    static XSSFWorkbook wb = null;
+
+
     public static void main(String[] args) {
         BasicConfigurator.configure();
-        XSSFWorkbook wb = null;
+
         try {
             wb = ExcelRead.get_Workbook("C:\\Users\\Mohammad Dallash\\Documents\\GitHub\\Advanced-Computer-Programming-Final-Project\\Excels\\basic.xlsx");
         } catch (IOException e) {
             System.out.println("err reading the file\n");
             return;
         }
-        String Cureent_cell;
-        String type;
-        String last_value;
-        Stack<JSONObject> stack = new Stack<>();
+
+
+
         JSONObject mainJSONObject = new JSONObject();
 
-        JSONObject JsonObj;
 
-
-
-        for (int i= 0; i <4; i++)
-        {
-            Cureent_cell = ExcelRead.read_Cell(i, 0, 0, wb).getStringCellValue();
-            type = ExcelRead.read_Cell(i, 1, 0, wb).getStringCellValue();
-            String [] array =Cureent_cell.split("/");
-            last_value = array[array.length - 1];
-
-
-
-            if ( type.equals("string") )
-            {
-                stack.peek().put(last_value , "string");
-            }
-            else
-            {
-                JSONObject JSONobject = new JSONObject();
-                stack.push(JSONobject);
-            }
-
-        }
-
-
-
-        mainJSONObject.put("object1" , stack.pop());
+        recursion("object1", mainJSONObject);
         System.out.println(mainJSONObject);
 
     }
+
+
+    public static void recursion(String lookFor, JSONObject JSONobject)
+    {
+        Cureent_cell = ExcelRead.read_Cell(i, 0, 0, wb).getStringCellValue();
+        type = ExcelRead.read_Cell(i, 1, 0, wb).getStringCellValue();
+        String [] array =Cureent_cell.split("/");
+        last_value = array[array.length - 1];
+        if (array.length <= 2 )  return;
+
+        if (type.equals("string"))
+        {
+            JSONobject.put(lookFor, last_value);
+            i++;
+            recursion(lookFor, JSONobject);
+        }
+        else
+        {
+            JSONObject inner = new JSONObject();
+            i++;
+            recursion(type, inner);
+            JSONobject.put(type, inner);
+        }
+
+
+    }
+
 
 
 }
