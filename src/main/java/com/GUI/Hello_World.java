941deeecba86c9e9b435132c72164930f6faa5c6
MohammadDallash <105324962+MohammadDallash@users.noreply.github.com>
2022-05-25 19:14:50 +0200

    good looking design

--- src/main/java/com/GUI/Hello_World.java
+++ src/main/java/com/GUI/Hello_World.java
@@ -6,11 +6,16 @@ import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Line;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.scene.layout.StackPane;
 public class Hello_World extends Application{
+    private static final int length = 800;
 
         public static void main (String[] args)
     {
@@ -25,12 +30,43 @@ public class Hello_World extends Application{
         stage.setScene(myScene);
 
 
-        stage.setWidth(420);
-        stage.setHeight(420);
+        stage.setWidth(length);
+        stage.setHeight(length);
         stage.setTitle("my Http program!");
         stage.setResizable(false);
 
 
+
+        Image background = new Image("C:\\Users\\Mohammad Dallash\\Documents\\GitHub\\Advanced-Computer-Programming-Final-Project\\src\\background.png");
+        ImageView background_img = new ImageView(background);
+        root.getChildren().add(background_img);
+
+
+
+
+
+        Rectangle myRectangle = new Rectangle();
+        myRectangle.setX(0.1*length);myRectangle.setY(0.1*length);
+        myRectangle.setWidth(0.8*length);myRectangle.setHeight(0.8*length);
+        myRectangle.setFill(Color.rgb(254,187,100));
+        myRectangle.setStrokeWidth(5);
+        myRectangle.setArcHeight(50);myRectangle.setArcWidth(50);
+        myRectangle.setStroke(Color.BLACK);
+        root.getChildren().add(myRectangle);
+
+
+        Line myLine = new Line();
+        myLine.setStartX(0.1*length);myLine.setStartY(0.25*length);
+        myLine.setEndX(0.9*length); myLine.setEndY(0.25*length);
+        myLine.setStrokeWidth(5);
+        myLine.setStroke(Color.BLACK);
+        root.getChildren().add(myLine);
+
+        Text myText = new Text("Name :" );
+        myText.setX(0.15*length);myText.setY(0.2*length);
+        myText.setFont(Font.font("Consolas", 0.0625*length));
+        myText.setFill(Color.BLACK);
+        root.getChildren().add(myText);
 
 
 
